3e34d71ef82ee367547ef9cdb62ad5d3c14427ff
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-08-27 14:01:21 +0530

    Bug fixes

--- Repository/RequestRepository.cs
+++ Repository/RequestRepository.cs
@@ -1,40 +1,40 @@
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 using API.Models;
 using Dapper;
-using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 public class RequestRepository {
     private static string connectionString;
-    public  RequestRepository() {
+    public RequestRepository () {
         connectionString = @"Server=192.168.8.103, 1433;Database=LOAN;User=SA;Password=Udara@123;ConnectRetryCount=0;MultipleActiveResultSets=true";
         // connectionString = @"Server=localhost;Database=LOAN;User=SA;Password=Ride2die;ConnectRetryCount=0;MultipleActiveResultSets=true";
     }
 
     public static IDbConnection Connection {
         get {
-            return new SqlConnection(connectionString);
+            return new SqlConnection (connectionString);
         }
     }
-
-    public static  void Add(Request request){
-        using  (IDbConnection dbConnection = Connection){
-             string sQuary = "INSERT INTO REQUESTS (rid,name,age,address,contact,job,ammount,duration,reason,pending,accepted,declined,nic,doc)"
-                            + "VALUES(@rid, @name, @age, @address, @contact, @job, @ammount, @duration, @reason, @pending, @accepted, @declined, @nic, @doc)";
-            dbConnection.Open();
-            dbConnection.Execute(sQuary, request);
+    //Add a Loan Request
+    public static void Add (Request request) {
+        using (IDbConnection dbConnection = Connection) {
+            dbConnection.Open ();
+            dbConnection.Query<Request> ("dbo.AddRequest",
+                request,
+                commandType : CommandType.StoredProcedure);
         }
     }
-
-    public  IEnumerable<Request> GetAll() {
-        using (IDbConnection dbConnection = Connection){
-            dbConnection.Open();
-            return dbConnection.Query<Request>("dbo.LoanRequests", 
-        commandType: CommandType.StoredProcedure);
+    //Get All Loan Requests
+    public IEnumerable<Request> GetAll () {
+        using (IDbConnection dbConnection = Connection) {
+            dbConnection.Open ();
+            return dbConnection.Query<Request> ("dbo.LoanRequests",
+                commandType : CommandType.StoredProcedure);
         }
     }
 
@@ -47,6 +47,7 @@ public class RequestRepository {
     //     }
     // }
 
+    // Update request for accept or decline Loan without SP
     public static void Update(Request request){
         using (IDbConnection dbConnection = Connection){
             var reason = request.reason;
@@ -60,4 +61,15 @@ public class RequestRepository {
             dbConnection.Query<Request>(sQuary, request);
         }
     }
+
+    // Update request for accept or decline Loan with SP
+
+    // public static void Update (Request request) {
+    //     using (IDbConnection dbConnection = Connection) {
+    //         dbConnection.Open ();
+    //         dbConnection.Query("dbo.EditRequest",
+    //             request,
+    //             commandType : System.Data.CommandType.StoredProcedure);
+    //     }
+    // }
 }
